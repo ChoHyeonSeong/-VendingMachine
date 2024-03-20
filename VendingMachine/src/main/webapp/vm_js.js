5eedd82b271f823355f4668d8982f8791221de2c
johs <johs970917@gmail.com>
2024-03-20 23:41:55 +0900

    코인 삽입, 반환 기능 추가

--- VendingMachine/src/main/webapp/vm_js.js
+++ VendingMachine/src/main/webapp/vm_js.js
@@ -1,7 +1,18 @@
 /**
  * 
  */
+let coinRemain;
 let balance = 0;
- function Insert100Coin(){
-	 balance+=100;
- }
+function Insert100Coin() {
+	balance += 100;
+	coinRemain.innerHTML = "<h2>" + balance + "</h2>";
+}
+function ReturnCoin(){
+	balance = 0;
+	coinRemain.innerHTML = "<h2>" + balance + "</h2>";
+}
+window.onload = () => {
+	document.getElementById("coininsert").onclick = Insert100Coin;
+	document.getElementById("coinreturn").onclick = ReturnCoin;
+	coinRemain = document.getElementById("coinremain");
+}
